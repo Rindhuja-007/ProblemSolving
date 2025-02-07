71a054fcdae7c4cb0e08c9176ab35899e26e58d4
Rindhuja chandramohan <145462351+Rindhuja-007@users.noreply.github.com>
2025-02-07 12:16:02 +0530

    Create 520. Detect Capital.py

---
+++ LeetCode/520. Detect Capital.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def detectCapitalUse(self, word):
+        if word.isupper():
+            return True
+        elif word.islower():
+            return True
+        elif word[0].isupper() and word[1:].islower():
+            return True
+    
+        return False
+        
